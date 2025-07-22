e6347e0be88e2de038e13d36842cddbd4f46ec76
jaymaina-8 <jaymaina19@gmail.com>
2025-07-22 09:44:43 +0300

    commit

--- src/Register.js
+++ src/Register.js
@@ -17,7 +17,7 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await axios.post("http://127.0.0.1:8000/api/auth/register/", form);
+            await axios.post("http://127.0.0.1:8000/api/register/", form);
             alert("Registration successful!");
             setForm({
                 full_name: "",
